dd32b38a433511d3f24daac5208e4e237e7ab40f
SexyLeoo <123617759+SexyLeoo@users.noreply.github.com>
2023-05-22 22:06:05 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cs
@@ -0,0 +1,25 @@
+using System;
+
+class B_Diary
+{
+    static void Main()
+    {
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+        int x = input[0];
+        int y = input[1];
+        int w = input[2];
+        int h = input[3];
+
+        int[] NumArray = new int[] { x, y, w - x, h - y };
+
+        int Min = NumArray[0];
+
+        for(int i=1;i<NumArray.Length;i++) 
+        {
+            if (Min > NumArray[i])
+                Min = NumArray[i];
+        }
+        Console.WriteLine(Min);
+    }
+}
