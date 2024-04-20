52034e3f9a8e316f4301b306bf75638d36b014e6
Leooo <123617759+Leooo918@users.noreply.github.com>
2024-04-20 14:21:28 +0900

    [Silver III] Title: 주유소, Time: 92 ms, Memory: 2800 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -5,7 +5,6 @@
 using namespace std;
 
 int n;
-int cost = 0;
 int city[100001];
 int road[100001];
 
@@ -19,34 +18,15 @@ int main()
 	for (int i = 1; i <= n; i++)
 		cin >> city[i];
 
-	int i = 1;
-	while (i <= n)
-	{
-		int lc = cost;
-		int cc = city[i];
-		int r = 0;
+	long m = city[1];
+	long cost = 0;
 
-		for (int j = i; j <= n; j++)
-		{
-			if (city[j] < cc)
-			{
-				cost += r * cc;
-				i = j;
-				break;
-			}
-			else
-			{
-				r += road[j];
-			}
+	for (int i = 1; i <= n - 1; i++) {
+		if (city[i] < m) {
+			m = city[i];
 		}
 
-		if (cost == lc)
-		{
-			for (int j = i; j <= n; j++)
-				cost += cc * road[j];
-
-			break;
-		}
+		cost += m * road[i];
 	}
 
 	cout << cost;
