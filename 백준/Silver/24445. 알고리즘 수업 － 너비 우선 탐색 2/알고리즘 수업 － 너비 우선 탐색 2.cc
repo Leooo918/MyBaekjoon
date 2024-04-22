953cae91dcdbfbb637eeb8910d828eb81d62c352
Leooo <123617759+Leooo918@users.noreply.github.com>
2024-04-23 08:32:46 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 92 ms, Memory: 8936 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+vector<int> vec[100001];
+int answer[100001];
+int cnt = 1;
+bool visited[100001];
+
+
+void dfs(int r)
+{
+	queue<int> q;
+	q.push(r);
+	answer[r] = cnt++;
+	visited[r] = true;
+
+	while (!q.empty())
+	{
+		int cn = q.front();
+		q.pop();
+
+		for (int i = 0; i < vec[cn].size(); i++)
+		{
+			int nr = vec[cn][i];
+
+			if (visited[nr]) continue;
+
+			q.push(nr);
+			answer[nr] = cnt++;
+			visited[nr] = true;
+		}
+	}
+}
+
+bool cmp(int a, int b)
+{
+	return a > b;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n, m, r;
+
+	cin >> n >> m >> r;
+
+	for (int i = 0; i < m; i++)
+	{
+		int u, v;
+		cin >> u >> v;
+
+		vec[u].push_back(v);
+		vec[v].push_back(u);
+	}
+
+	for (int i = 1; i <= n; i++)
+		sort(vec[i].begin(), vec[i].end(), cmp);
+
+	dfs(r);
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << answer[i] << '\n';
+	}
+}
