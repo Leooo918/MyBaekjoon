c2b546e7fa2b6d2c3881e39be8ad1a75237b918c
Leooo <123617759+Leooo918@users.noreply.github.com>
2024-05-22 11:54:45 +0900

    [Silver IV] Title: 수 찾기, Time: 56 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m;
+	vector<int> A;
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		A.push_back(a);
+	}
+
+	sort(A.begin(), A.end());
+
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int b;
+		cin >> b;
+
+
+		int start = 0;
+		int end = n - 1;
+		int mid;
+
+		bool isIt = false;
+		while (end >= start)
+		{
+			mid = (start + end) / 2;
+			if (A[mid] == b)
+			{
+				cout << 1 << "\n";
+				isIt = true;
+				break;
+			}
+			else if (A[mid] > b) end = mid - 1;
+			else start = mid + 1;
+		}
+		if (isIt == false) {
+			cout << 0 << "\n";
+		}
+	}
+}
