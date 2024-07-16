9b9852e847d54ad25ea97b9f5df1503291f5192e
Leooo <123617759+Leooo918@users.noreply.github.com>
2024-07-16 16:11:01 +0900

    [Silver II] Title: 스승님 찾기, Time: 0 ms, Memory: 2020 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/15979. 스승님 찾기/스승님 찾기.cc
+++ 백준/Silver/15979. 스승님 찾기/스승님 찾기.cc
@@ -4,17 +4,27 @@
 
 using namespace std;
 
-int n, m;
+
+long long int f(long long int a, long long int b)
+{
+	if (a == 0 || b == 0) return a + b;
+	if (a > b) return f(a % b, b);
+	return f(b % a, a);
+}
 
 int main()
 {
+	long long int n, m;
 	cin >> n >> m;
+
+	if (n < 0) n *= -1;
+	if (m < 0) m *= -1;
 
 	if (n == 0 && m == 0)
 	{
 		cout << 0;
 	}
-	else if (n == 1 || n == -1 || m == 1 || m == -1)
+	else if (f(n, m) == 1)
 	{
 		cout << 1;
 	}
