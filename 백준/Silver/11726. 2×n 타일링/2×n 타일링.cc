7f350b6664aaf79cc347ccdf39e3d06a31817427
Leooo <123617759+Leooo918@users.noreply.github.com>
2024-08-06 00:36:42 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int dp[1001];
+    cin >> n;
+    
+    dp[0] = 1;
+    dp[1] = 2;
+    
+    for(int i = 2; i < n; i++)
+    {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+    
+    cout << dp[n - 1];
+}
