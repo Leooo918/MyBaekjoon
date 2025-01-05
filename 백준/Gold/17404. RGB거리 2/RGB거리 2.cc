237081dc5826beeebfce4566d2a61d04218c64e3
Leooo <123617759+Leooo918@users.noreply.github.com>
2025-01-05 13:08:08 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -21,18 +21,6 @@ int main()
 		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
 	}
 
-	dp[1][0] = arr[1][0];
-	dp[1][1] = INT16_MAX;
-	dp[1][2] = INT16_MAX;
-
-	for (int j = 2; j <= n; j++)
-	{
-		dp[j][0] = arr[j][0] + min(dp[j - 1][1], dp[j - 1][2]);
-		dp[j][1] = arr[j][1] + min(dp[j - 1][0], dp[j - 1][2]);
-		dp[j][2] = arr[j][2] + min(dp[j - 1][0], dp[j - 1][1]);
-	}
-	answer = min(dp[n][1], dp[n][2]);
-
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
@@ -48,7 +36,7 @@ int main()
 			dp[j][2] = arr[j][2] + min(dp[j - 1][0], dp[j - 1][1]);
 		}
 
-		if (i == 0) answer = min(answer, min(dp[n][1], dp[n][2]));
+		if (i == 0) answer = min(dp[n][1], dp[n][2]);
 		if (i == 1) answer = min(answer, min(dp[n][0], dp[n][2]));
 		if (i == 2) answer = min(answer, min(dp[n][0], dp[n][1]));
 	}
