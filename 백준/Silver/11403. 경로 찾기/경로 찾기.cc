181747f665ce322b263e90752ffaa046e3f73845
Leooo <123617759+Leooo918@users.noreply.github.com>
2025-04-08 21:13:14 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int a[101][101];
+
+
+int main(int argc, char** argv)
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+		}
+	}
+
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (a[i][k] == 1 && a[k][j] == 1) {
+					a[i][j] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << a[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
