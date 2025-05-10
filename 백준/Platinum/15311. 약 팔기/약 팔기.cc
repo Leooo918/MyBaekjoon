0d66453b0e5f2b12780bb5bc51a6fc1a4211a988
Leooo <123617759+Leooo918@users.noreply.github.com>
2025-05-10 11:43:30 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	cout << 2000 << '\n';
+	for (int i = 0; i < 1000; i++)
+		cout << 1 << " ";
+	for (int i = 0; i < 1000; i++)
+		cout << 1000 << " ";
+}
