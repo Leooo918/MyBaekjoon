61ccc42264a02eea90a02fc8a19b0a228f83032a
Leooo <123617759+Leooo918@users.noreply.github.com>
2025-05-15 23:21:44 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    
+    string a, b;
+    cin >> a >> b;
+    
+    if(a.size() >= b.size())
+    {
+        cout << "go";
+    }
+    else
+    {
+        cout << "no";
+    }
+}
